95093af476945ff60fd5b839d62716fb214c5b47
Mujtaba <mujtaba56383@gmail.com>
2025-07-06 16:31:12 +0500

    trying to fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './router/AppRouter';
+import AppRouter from './router/AppRouter.jsx';
 import Navbar from './Components/Navbar'; 
 import Footer from './Components/Footer'
 
