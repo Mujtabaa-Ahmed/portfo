47d39da9970da50d80bc56f0136a757b8484bca8
Mujtaba <123797571+Mujtabaa-Ahmed@users.noreply.github.com>
2025-07-08 10:30:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './router/AppRouter.jsx';
+import AppRouter from './Router/AppRouter.jsx';
 import Navbar from './components/Navbar.jsx'; 
 import Footer from './components/Footer.jsx'
 
